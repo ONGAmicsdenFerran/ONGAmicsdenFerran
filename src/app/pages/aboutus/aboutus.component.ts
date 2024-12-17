1e32962eabaf6b5ed85722cc430ed41e32ae660a
ONGAmicsdenFerran <ongamicsdenferran@gmail.com>
2024-12-17 20:31:57 +0100

    Añado el <app-header> a las páginas aboutus y home

--- src/app/pages/aboutus/aboutus.component.ts
+++ src/app/pages/aboutus/aboutus.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterModule} from "@angular/router";
+import {HeaderComponent} from "../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-aboutus',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    HeaderComponent
   ],
   templateUrl: './aboutus.component.html',
   styleUrl: './aboutus.component.scss'
