e9ff679e299ce55c4b21c3f5f50ecbe7b299c67a
ONGAmicsdenFerran <ongamicsdenferran@gmail.com>
2024-12-18 14:05:12 +0100

    Añado los nombres de las actividades

--- src/app/pages/activitats/activitats.component.ts
+++ src/app/pages/activitats/activitats.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../../shared/components/header/header.component";
 import {FooterComponent} from "../../shared/components/footer/footer.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-activitats',
   standalone: true,
-    imports: [
-        HeaderComponent,
-        FooterComponent
-    ],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    RouterLink
+  ],
   templateUrl: './activitats.component.html',
   styleUrl: './activitats.component.scss'
 })
